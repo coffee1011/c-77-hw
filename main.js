1b5b1496ca7cc75b4da06b1ab3e8fcdb906c65a4
coffee1011 <84262484+coffee1011@users.noreply.github.com>
2022-01-10 17:38:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+var first_name = "aditya";
+console.log(first_name);
+
+var last_name = "chopra";
+console.log(last_name);
+
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+
+var a = 34;
+console.log(a);
+
+var b = 22;
+console.log(b);
+
+var add = a + b;
+console.log(add);
+
+var minus = a - b;
+console.log(minus);
+
+var multiply = a * b;
+console.log(multiply);
+
+var divison = a / b;
+console.log(divison);
